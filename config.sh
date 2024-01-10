db179be37a53b77b7fab25b9c83db3b7ef13c69f
Florent Lamiraux <florent@laas.fr>
2024-01-10 08:14:57 +0100

    [config] Update to conda installation.

--- config.sh
+++ config.sh
@@ -1,6 +1,1 @@
-export PYTHONPATH=/usr/local/insa/openrobots/lib/python2.7/site-packages
-export LD_LIBRARY_PATH=/usr/local/insa/openrobots/lib
-export PATH=/usr/local/insa/openrobots/bin:/usr/local/insa/openrobots/sbin:/bin
-
-alias python='/usr/bin/python'
-alias ipython='/usr/bin/ipython'
+export ROS_PACKAGE_PATH=${PWD}/script
